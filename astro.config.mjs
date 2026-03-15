6469a1430b5d4866517b571303be279e765f9256
jeorp <chaco1305@gmail.com>
2026-03-15 19:27:18 +0900

    add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://jeorp.github.io',
+	base: 'carrier-compass',
 	integrations: [mdx(), sitemap()],
 });
